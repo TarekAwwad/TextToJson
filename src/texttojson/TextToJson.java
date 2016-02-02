57fcc4441d021a7072f16ef62fd761df21ada52a
TarekAwwad <tarek_awwad@liris501-3-r34.insa-lyon.fr>
2016-02-02 09:30:59 +0100

    Minors - remove unnecessary lines

--- src/texttojson/TextToJson.java
+++ src/texttojson/TextToJson.java
@@ -9,10 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  *
